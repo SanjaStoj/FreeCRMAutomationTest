82e20d5e5cf5ad3d7eef4fb540570bef3bb98c13
Sanja Stojanovska <svasiljevska@yahoo.com>
2023-05-30 22:42:20 +0200

    First commit to github

--- src/main/java/pages/DealsPage.java
+++ src/main/java/pages/DealsPage.java
@@ -2,5 +2,6 @@ package pages;
 
 public class DealsPage {
 
-    //changes test testhttps://github.com/SanjaStoj/FreeCRMAutomation.git
+    //changes test test https://github.com/SanjaStoj/FreeCRMAutomation.git
+    //https://github.com/SSanjaTest/FreeCRMAutomationTest.git
 }
